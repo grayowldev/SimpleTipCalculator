ea5a5fa97710848dbe46737d4a8e87076018b400
GrayOwl <truegrayowl@gmail.com>
2017-05-26 18:32:37 -0400

    more minor improvements[buggy]

--- app/src/main/java/strada/com/simpletipcalculator/MainActivity.java
+++ app/src/main/java/strada/com/simpletipcalculator/MainActivity.java
@@ -10,9 +10,10 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     TextView billAmtView, totalView, totalEachView,eachTV;
-    Button billAmtBtn;
+    Button billAmtBtn,perc0,perc5,perc10,perc15,perc20,perc25;
     int billAmt = 0;
-    int total, totalEach;
+    int totalEach;
+    Double total,tipPercentage,tipAmt;
 
 
 
@@ -21,12 +22,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tipPercentage = 0.0;
+
+
         // view init
         billAmtView = (TextView)findViewById(R.id.bill_amt_view);
         totalView = (TextView)findViewById(R.id.total_view);
         eachTV = (TextView) findViewById(R.id.each_tv);
         totalEachView = (TextView) findViewById(R.id.total_ind_view);
         billAmtBtn = (Button) findViewById(R.id.bill_amt_edit_btn_onHome);
+        perc0 = (Button) findViewById(R.id.perc0);
+        perc5 = (Button) findViewById(R.id.perc5);
+        perc10 = (Button) findViewById(R.id.perc10);
+        perc15 = (Button) findViewById(R.id.perc15);
+        perc20 = (Button) findViewById(R.id.perc20);
+        perc25 = (Button) findViewById(R.id.perc25);
+
 
 
         //  OnClicks
@@ -34,6 +45,84 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, BillAmtInputActivity.class));
+            }
+        });
+
+        findViewById(R.id.perc0).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                perc0.setTextSize(30);
+                perc5.setTextSize(18);
+                perc10.setTextSize(18);
+                perc15.setTextSize(18);
+                perc20.setTextSize(18);
+                perc25.setTextSize(18);
+                tipPercentage = 0.00;
+            }
+        });
+
+        findViewById(R.id.perc5).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                perc0.setTextSize(18);
+                perc5.setTextSize(30);
+                perc10.setTextSize(18);
+                perc15.setTextSize(18);
+                perc20.setTextSize(18);
+                perc25.setTextSize(18);
+                tipPercentage = 0.05;
+            }
+        });
+
+        findViewById(R.id.perc10).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                perc0.setTextSize(18);
+                perc5.setTextSize(18);
+                perc10.setTextSize(30);
+                perc15.setTextSize(18);
+                perc20.setTextSize(18);
+                perc25.setTextSize(18);
+                tipPercentage = 0.1;
+            }
+        });
+
+        findViewById(R.id.perc15).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                perc0.setTextSize(18);
+                perc5.setTextSize(18);
+                perc10.setTextSize(18);
+                perc15.setTextSize(30);
+                perc20.setTextSize(18);
+                perc25.setTextSize(18);
+                tipPercentage = 0.15;
+            }
+        });
+
+        findViewById(R.id.perc20).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                perc0.setTextSize(18);
+                perc5.setTextSize(18);
+                perc10.setTextSize(18);
+                perc15.setTextSize(18);
+                perc20.setTextSize(30);
+                perc25.setTextSize(18);
+                tipPercentage = 0.20;
+            }
+        });
+
+        findViewById(R.id.perc25).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                perc0.setTextSize(18);
+                perc5.setTextSize(18);
+                perc10.setTextSize(18);
+                perc15.setTextSize(18);
+                perc20.setTextSize(18);
+                perc25.setTextSize(30);
+                tipPercentage = 0.25;
             }
         });
 
@@ -48,7 +137,9 @@ public class MainActivity extends AppCompatActivity {
             billAmtBtn.setText(String.valueOf(billAmt));
         }
 
-        total = billAmt;
+        total = Double.valueOf(billAmt); // Temporal fix
+        tipAmt = billAmt*tipPercentage;
+        total = billAmt + tipAmt;
         totalView.setText("Total " + total);
 
         if (total == totalEach){
@@ -64,4 +155,6 @@ public class MainActivity extends AppCompatActivity {
     public double getBillAmt() {
         return Double.valueOf(billAmtView.getText().toString());
     }
+
+    
 }
